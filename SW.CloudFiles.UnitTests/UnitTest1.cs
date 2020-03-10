ebeab0ce1cc2af9e336ca6890d0f9fb20043062d
Samer Zughul <samerz@simplify9.com>
2020-03-10 16:14:51 +0200

    lifecycle mgmt plus write file config

--- SW.CloudFiles.UnitTests/UnitTest1.cs
+++ SW.CloudFiles.UnitTests/UnitTest1.cs
@@ -45,11 +45,9 @@ namespace SW.CloudFiles.UnitTests
         {
             var cloudFiles = server.Host.Services.GetService<CloudFilesService>();
 
-            using (var readWrapper = await cloudFiles.OpenReadAcync("test/sample.txt"))
-            using (var diskFile = File.OpenWrite(@"c:\temp\sample.txt"))
-            {
-                await readWrapper.Stream.CopyToAsync(diskFile);
-            }
+            using var readWrapper = await cloudFiles.OpenReadAcync("test/sample.txt");
+            using var diskFile = File.OpenWrite(@"c:\temp\sample.txt");
+            await readWrapper.Stream.CopyToAsync(diskFile);
         }
 
         [TestMethod]
@@ -58,11 +56,16 @@ namespace SW.CloudFiles.UnitTests
             var cloudFiles = server.Host.Services.GetService<CloudFilesService>();
 
             //using (Stream cloudStream = await cloudFiles.OpenReadAcync("test/sample.txt"))
-            using (var diskFile = File.OpenRead(@"c:\temp\sample.txt"))
+            using var diskFile = File.OpenRead(@"c:\temp\sample.txt");
+            await cloudFiles.WriteAcync(diskFile, new WriteFileSettings
             {
-                await cloudFiles.WriteAcync("test/TestWriteAcync.txt", diskFile);
-            }
+                Key = "test/TestWriteAcync.txt",
+                ContentType="plain/text",
+                Public = false
+            });
         }
+
+
 
         [TestMethod]
         async public Task TestOpenWriteAsync()
@@ -70,14 +73,11 @@ namespace SW.CloudFiles.UnitTests
             var cloudFiles = server.Host.Services.GetService<CloudFilesService>();
 
             //using (Stream cloudStream = await cloudFiles.OpenReadAcync("test/sample.txt"))
-            using (var writeWrapper = await cloudFiles.OpenWriteAsync("test/TestOpenWriteAsync.txt"))
-            using (var textWriter = new StreamWriter(writeWrapper.Stream))
-            {
-                textWriter.Write("hello");
-                textWriter.Flush(); 
-                await writeWrapper.CompleteRequestAsync();
-                //streamWrapper.Close();  
-            }
+            using var writeWrapper = await cloudFiles.OpenWriteAsync("test/TestOpenWriteAsync.txt");
+            using var textWriter = new StreamWriter(writeWrapper.Stream);
+            textWriter.Write("hello");
+            textWriter.Flush();
+            await writeWrapper.CompleteRequestAsync();
         }
 
 
--- SW.CloudFiles/Extensions/IServiceCollectionExtensions.cs
+++ SW.CloudFiles/Extensions/IServiceCollectionExtensions.cs
@@ -3,8 +3,11 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Util;
 using Microsoft.Extensions.DependencyInjection;
+using Nito.AsyncEx;
+using Nito.AsyncEx.Synchronous;
 using System;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SW.CloudFiles
 {
@@ -17,18 +20,62 @@ namespace SW.CloudFiles
 
             using (var client = cloudFilesOptions.CreateClient())
             {
-                if (!AmazonS3Util.DoesS3BucketExistV2Async(client, cloudFilesOptions.BucketName).Result)
+
+
+
+                var task = Task.Run(async () => await AmazonS3Util.DoesS3BucketExistV2Async(client, cloudFilesOptions.BucketName));
+
+                if (!AmazonS3Util.DoesS3BucketExistV2Async(client, cloudFilesOptions.BucketName).WaitAndUnwrapException())
                 {
                     var putBucketRequest = new PutBucketRequest
                     {
                         BucketName = cloudFilesOptions.BucketName,
-                        //UseClientRegion = true,
-
                         CannedACL = S3CannedACL.Private
                     };
+                    _ =  client.PutBucketAsync(putBucketRequest).WaitAndUnwrapException();
 
-                    PutBucketResponse putBucketResponse = client.PutBucketAsync(putBucketRequest).Result;
                 }
+
+                 var config = client.GetLifecycleConfigurationAsync(new GetLifecycleConfigurationRequest
+                {
+                    BucketName = cloudFilesOptions.BucketName
+                }).WaitAndUnwrapException().Configuration;
+
+                if (config.Rules.Count == 0)
+                {
+
+                    client.PutLifecycleConfigurationAsync(new PutLifecycleConfigurationRequest
+                    {
+                        BucketName= cloudFilesOptions.BucketName,
+                        Configuration = new LifecycleConfiguration
+                        {
+                            Rules = new List<LifecycleRule>
+                            {
+                                new LifecycleRule
+                                {
+                                    Id = "temp1",
+                                    Expiration = new LifecycleRuleExpiration { Days=1},
+                                    Prefix = "temp1/",
+                                },
+                                new LifecycleRule
+                                {
+                                    Id = "temp7",
+                                    Expiration = new LifecycleRuleExpiration { Days = 7 },
+                                    Prefix = "temp7/",
+                                },
+                                new LifecycleRule
+                                {
+                                    Id = "temp30",
+                                    Expiration = new LifecycleRuleExpiration { Days = 30 },
+                                    Prefix = "temp30/",
+                                }
+
+                            }
+                        }
+                    }).WaitAndUnwrapException();
+
+                };
+
 
             }
 
